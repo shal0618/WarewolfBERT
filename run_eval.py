18531373c90e8d0d3c15f96fe82e83452ca73178
shal0618 <m17124@g.metro-cit.ac.jp>
2021-08-25 18:37:42 +0900

    initial commit

---
+++ run_eval.py
@@ -0,0 +1,27 @@
+import torch
+
+from config import Config
+from nn import build_model
+from tokenizer import Tokenizer
+from utils import evaluate
+
+if __name__ == '__main__':
+
+    # device = torch.device(Config.device)
+    device = torch.device('cpu')
+
+    state_dict = torch.load(f'{Config.data_dir}/{Config.fn}.pth', map_location=device)
+
+    tokenizer = Tokenizer.from_pretrained(Config.model_name)
+
+    model = build_model(Config).to(device)
+    model.load_state_dict(state_dict['model'])
+    model.eval()
+    model.freeze()
+
+    while True:
+        s = input('You>')
+        if s == 'q':
+            break
+        print('BOT>', end='')
+        text = evaluate(Config, s, tokenizer, model, device, True)
