3849aec893ffb5dab83f7fa6cf2adc6e9a4e03bb
shal0618 <m17124@g.metro-cit.ac.jp>
2021-12-21 00:40:22 +0900

    add

---
+++ data/DL_ckpt.sh
@@ -0,0 +1,5 @@
+FILE_ID=1X7fqr_8UBVbMtirjUr0Ew_UlirL6eu3w
+FILE_NAME=dialog.zip
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o ${FILE_NAME}
